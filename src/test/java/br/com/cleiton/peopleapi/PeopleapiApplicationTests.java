036376cced9082ed6bf108e8926c3923f02b1db1
Cleiton Socodolski <cleitonjosimar@brq.com>
2021-11-10 10:43:04 -0300

    estrutura inicial

---
+++ src/test/java/br/com/cleiton/peopleapi/PeopleapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cleiton.peopleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
